747bb104c5b69dba4236d70ffb1307c090e278b5
TiciaChance <laticia.chance@flatironschool.com>
2016-06-09 09:44:25 -0400

    Done.

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -6,35 +6,44 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
     return YES;
+    
 }
 
 - (NSArray *)holidaysInSeason:(NSString *)season {
     
-    return nil;
+    
+    [self database];
+    return [_database[season]allKeys];
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
     
-    return nil;
+    return _database[season][holiday];
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
     
-    return nil;
+    return [[_database[season] allKeys] containsObject:holiday];
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
     
-    return nil;
+    return [_database[season][holiday] containsObject:supply];
 }
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
+    
+    //give holiday a new value of an empty mutable array
+    
+    NSMutableArray *forHolidayValue = [[NSMutableArray alloc] init];
+    _database[season][holiday] = forHolidayValue;
     
     // no return
 }
@@ -42,6 +51,8 @@
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
+    
+    [_database[season][holiday]addObject:supply];
     
     // no return
 }
